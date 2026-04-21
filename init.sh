716b2e9c6e36b77548fa35c033b7c4d874d1a39b
MerrickJC <126678877+MerrickJC@users.noreply.github.com>
2026-04-21 20:28:02 +1000

    2.1.1 - Updating docker compose files

--- init.sh
+++ init.sh
@@ -1,1 +1,7 @@
+# Changes the default githooks path for just this repository. For more about githooks see: https://git-scm.com/book/en/v2/Customizing-Git-Git-Hooks
+# By changing the githooks path, we can version control our githooks along with the rest of our code
 git config core.hooksPath .githooks
+
+# A docker image used in the backend Dockerfile. It is indirectly called by the docker-compose.yml
+# file and causes issues, so it is easier to pull directly first
+docker pull alpine/git:v2.52.0
