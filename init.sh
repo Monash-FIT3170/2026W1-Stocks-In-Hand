2753455f834f76302edea3e4ced6a6f758e9aa70
MerrickJC <126678877+MerrickJC@users.noreply.github.com>
2026-04-23 11:06:50 +1000

    2.1.1 - Updating init script to prevent Windows from converting file line endings

--- init.sh
+++ init.sh
@@ -2,6 +2,9 @@
 # By changing the githooks path, we can version control our githooks along with the rest of our code
 git config core.hooksPath .githooks
 
+# This prevents windows from converting files to a CRLF line ending, which makes them unreadable by the docker images
+git config core.autocrlf false
+
 # A docker image used in the backend Dockerfile. It is indirectly called by the docker-compose.yml
 # file and causes issues, so it is easier to pull directly first
 docker pull alpine/git:v2.52.0
