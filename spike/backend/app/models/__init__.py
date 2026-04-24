5743500bd22d2b241b8595d88afa2213956785a7
apor0014 <apor0014@student.monash.edu>
2026-04-24 22:19:14 +1000

    <2>.<1>.<2> <86d2q19hc> Added Schema and Design for watchlist

--- spike/backend/app/models/__init__.py
+++ spike/backend/app/models/__init__.py
@@ -3,3 +3,4 @@ from .claim_source import ClaimSource
 from .report import Report
 from .report_claim import ReportClaim
 from .user import User
+from .watchlist import Watchlist
--- spike/backend/app/schemas/__init__.py
+++ spike/backend/app/schemas/__init__.py
@@ -3,3 +3,4 @@ from .claim_source import ClaimSourceCreate, ClaimSourceResponse
 from .report import ReportCreate, ReportResponse
 from .report_claim import ReportClaimCreate, ReportClaimResponse
 from .user import UserCreate, UserResponse
+from .watchlist import WatchlistCreate, WatchlistResponse
