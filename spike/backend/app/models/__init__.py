51e2b5f91cc2a8a41173d3b2f3ac1a03c5d97a4d
apor0014 <apor0014@student.monash.edu>
2026-04-24 22:17:06 +1000

    <2>.<1>.<2> <86d2q19hc> Added reports model and schema with necessary fields

--- spike/backend/app/models/__init__.py
+++ spike/backend/app/models/__init__.py
@@ -1,3 +1,4 @@
 from .claim import Claim
 from .claim_source import ClaimSource
+from .report import Report
 from .user import User
--- spike/backend/app/schemas/__init__.py
+++ spike/backend/app/schemas/__init__.py
@@ -1,3 +1,4 @@
 from .claim import ClaimCreate, ClaimResponse
 from .claim_source import ClaimSourceCreate, ClaimSourceResponse
+from .report import ReportCreate, ReportResponse
 from .user import UserCreate, UserResponse
