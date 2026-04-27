0f8266bd780d570d61e53d696dce6ac503a6fe08
MerrickJC <126678877+MerrickJC@users.noreply.github.com>
2026-04-27 17:40:38 +1000

    2.1.1 - Updating python tests to output to file

--- backend/tests.sh
+++ backend/tests.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# Redirecting all terminal output from this script to go to an output file
+# Creating timestamp folder 
+printf -v date '%(%Y-%m-%d-%H-%M-%S)T' -1
+folder="/test_output/${date}"
+mkdir $folder
+
+# Save stdout to file descriptor (so it can be restored later)
+exec 3>&1
+exec > "$folder/_errors.txt" 2>&1
+
+
 # checks for code style
 echo "---------- PYLINT ----------"
-pylint --recursive=y .
+pylint --recursive=y . > $folder/pylint.txt
+
 
 # checks for type annotations
 echo "---------- MYPY ----------"
@@ -12,12 +24,24 @@ mypy . \
     --check-untyped-defs \
     --ignore-missing-imports \
     --no-strict-optional \
-    --pretty
+    --pretty > $folder/mypy.txt
+
 
 # runs unit tests and reports code coverage
 echo "---------- PYTEST ----------"
-coverage run -m pytest
+coverage run -m pytest > $folder/pytest.txt
+
 
 # searches for code security vulnerabilities
 echo "---------- BANDIT ----------"
-bandit -c bandit.yaml -r .
+bandit -c bandit.yaml -r . > $folder/bandit.txt
+
+
+# Restore output from file descriptor 3
+exec 1>&3
+exec 2>&3
+
+# Close file descriptor 3 (cleanup)
+exec 3>&-
+
+echo "TEST SCRIPT COMPLETE, OUTPUT CAN BE FOUND IN '$folder'"
